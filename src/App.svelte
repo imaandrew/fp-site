2083c10dd1058f3338d9cdc4fa86514fe90a4edd
imaandrew <14946103+imaandrew@users.noreply.github.com>
2023-06-16 17:55:48 -0400

    Add formatter

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,8 @@
-<h1>FP injection site</h1>
-
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { getTags } from './lib/github';
-  import { getMd5 } from './lib/md5';
-  import Select from 'svelte-select';
+  import { onMount } from "svelte";
+  import { getTags } from "./lib/github";
+  import { getMd5 } from "./lib/md5";
+  import Select from "svelte-select";
   let fileName: string;
   let fileInput: HTMLInputElement;
   let ver: Version;
@@ -45,17 +43,22 @@
     }
   };
 
-  const buildFp = () => {
-
-  };
-
+  const buildFp = () => {};
 </script>
+
+<h1>FP injection site</h1>
 
 <div class="container">
   <div style="display: flex; align-items: center;">
-    <label for="fileInput" style="text-align: left; margin-right: 3%;">ROM:</label>
-    <button class="fileButton" on:click={() => fileInput.click()}>Open File</button>
-    <label for="fileInput" style="text-align: left; margin-left: 3%;">{fileName || 'No file selected.'}</label>
+    <label for="fileInput" style="text-align: left; margin-right: 3%;"
+      >ROM:</label
+    >
+    <button class="fileButton" on:click={() => fileInput.click()}
+      >Open File</button
+    >
+    <label for="fileInput" style="text-align: left; margin-left: 3%;"
+      >{fileName || "No file selected."}</label
+    >
   </div>
   <p>{romHashMessage}</p>
   <input
@@ -65,40 +68,49 @@
     on:change={handleFileSelect}
     style="display: none;"
   />
-  
+
   <div style="display: flex; align-items: center;">
     <p style="text-align: left; margin-right: 3%;">Version:</p>
-    <Select loadOptions={getTags} --background="black" --list-background="black" --item-hover-color="black"/>
+    <Select
+      loadOptions={getTags}
+      --background="black"
+      --list-background="black"
+      --item-hover-color="black"
+    />
   </div>
   <div class="radio-buttons">
     <label for="n64">
-      <input type="radio" id="n64" name="platform" value="N64">
+      <input type="radio" id="n64" name="platform" value="N64" />
       N64
     </label>
-  
+
     <label for="wii">
-      <input type="radio" id="wii" name="platform" value="Wii">
+      <input type="radio" id="wii" name="platform" value="Wii" />
       Wii
     </label>
-  
+
     <label for="wiiu">
-      <input type="radio" id="wiiu" name="platform" value="Wii U">
+      <input type="radio" id="wiiu" name="platform" value="Wii U" />
       Wii U
     </label>
-  
+
     <label for="switch">
-      <input type="radio" id="switch" name="platform" value="Switch">
+      <input type="radio" id="switch" name="platform" value="Switch" />
       Switch
     </label>
   </div>
   <div class="asdfasdf">
     <label for="outfile">Output file:</label>
-    <input type="text" style="height:30px; width:300px; margin-left: 3%" id="outfile" name="outfile"><br><br>
+    <input
+      type="text"
+      style="height:30px; width:300px; margin-left: 3%"
+      id="outfile"
+      name="outfile"
+    /><br /><br />
   </div>
 
   <button class="injectButton" on:click={() => buildFp()}>Build</button>
 </div>
-
 
 <style>
   .container {
@@ -126,10 +138,10 @@
   }
 
   .injectButton {
-    background-color: seagreen
+    background-color: seagreen;
   }
-  
+
   .asdfasdf {
-    padding-top: 5%
+    padding-top: 5%;
   }
 </style>
