3e8b23b18cd4c671a9c15cb4a529af45c007b65c
imaandrew <14946103+imaandrew@users.noreply.github.com>
2023-07-19 23:31:15 -0400

    Fix update to prettier

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -11,7 +11,7 @@ export async function getS3File(path: string): Promise<Uint8Array> {
 export async function getTags(): Promise<string[]> {
   console.log("making request");
   const response = await fetch(
-    "https://api.github.com/repos/jcog/fp/git/refs/tags"
+    "https://api.github.com/repos/jcog/fp/git/refs/tags",
   );
   const data = await response.json();
   return data.map((item: { ref: string }) => item.ref.split("/").pop());
