01e76df29c0b463401b0f400907cf32ade735f2c
imaandrew <14946103+imaandrew@users.noreply.github.com>
2024-04-12 19:56:17 -0400

    Switch to vite-plugin-wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
-import wasmPack from "vite-plugin-wasm-pack";
+import wasm from "vite-plugin-wasm";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), wasmPack([], ["fp-web-patcher"]), topLevelAwait()],
+  plugins: [svelte(), wasm(), topLevelAwait()],
 });
